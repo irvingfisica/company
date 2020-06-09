97609587550388de1f0d99af273cedd42cb7e07b
Irving Morales <irvingfisica@gmail.com>
2020-06-09 18:24:09 -0500

    Código listo

--- src/main.rs
+++ src/main.rs
@@ -22,16 +22,20 @@ fn main() {
 
     	departamento = departamento.trim().to_string();
 
-    	println!("{:?}",departamento);
-
     	if departamento == String::from("0") {
     		break;
     	}
+
+    	// if departamento == String::from("2") {
+    	// 	empleados_lista(&database);
+    	// }
 
     	loop {
 
     		println!("El departamento seleccionado es: {}", &departamento);
     		println!("Para seleccionar otro presione 0");
+    		println!("Para listar a todos en {} presione 1", &departamento);
+    		println!("Para listar a todos en la empresa presione 2");
     		println!("Introduzca el nombre del empleado...");
 
     		empleado = String::new();
@@ -42,18 +46,47 @@ fn main() {
 
     		empleado = empleado.trim().to_string();
 
-    		println!("{:?}",empleado);
-
-    		if empleado == "0" {
-    			break;
+    		match &empleado[..] {
+    			"0" => break,
+    			"1" => match database.get(&departamento) {
+    				Some(vector) => {
+    					let mut vector_copy = vector.clone();
+    					vector_copy.sort();
+    					println!("{:?}",vector_copy);
+    				},
+    				None => (),
+    			},
+    			"2" => empleados_lista(&database),
+    			_ => {
+    				let vector = database.entry(departamento.clone()).or_insert(vec![]);
+    				vector.push(empleado);
+    			},
     		}
 
-    		let vector = database.entry(departamento.clone()).or_insert(vec![]);
-    		vector.push(empleado);
     	}
 
     }
 
-    println!("{:?}",database);
+}
 
+fn empleados_lista(mapa: &HashMap<String,Vec<String>>) {
+
+	let mut deptos = vec![];
+
+	for (key, _value) in mapa {
+		deptos.push(key);
+	}
+
+	deptos.sort();
+
+	for depto in &deptos {
+		let mut vectemp = mapa.get(*depto).unwrap().clone();
+		vectemp.sort();
+
+		println!("Los emplados en {} son:", depto);
+
+		for empleado in &vectemp {
+			println!("{}",&empleado);
+		}
+	}
 }
